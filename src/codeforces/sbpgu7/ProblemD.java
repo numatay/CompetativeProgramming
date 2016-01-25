aad33e14a2bc92b6a5e8230bc4fb65b72097d751
Andrey Biryulin <andrey.biryulin@greenapple.kz>
2016-01-25 20:27:08 +0600

    Added initial code for problem D

---
+++ src/codeforces/sbpgu7/ProblemD.java
@@ -0,0 +1,41 @@
+package codeforces.sbpgu7;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+public class ProblemD {
+
+	public static void main(String[] args) {
+		
+		ProblemD p = new ProblemD();
+		Solution s = p.new Solution();
+		try {
+			s.solve();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private class Solution {
+		public void solve() throws Exception {
+			//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new FileReader("in.txt")); 
+		PrintStream ps = new PrintStream(System.out);
+		
+		/* solution */
+		
+		
+		
+		
+		
+		
+		
+		/* cleaning up */
+		ps.close();
+		}
+	}
+	
+}
