4144692b8fc3cb58af356fae2b7450e9131a3be0
Nurgissa Umatay <nurgisa@gmail.com>
2016-01-25 23:01:35 +0600

    Redesigned Skeleton class, solved problemA

--- src/codeforces/sbpgu7/ProblemA.java
+++ src/codeforces/sbpgu7/ProblemA.java
@@ -8,69 +8,64 @@ import java.io.PrintStream;
 import java.io.PrintWriter;
 
 public class ProblemA {
+    public void solve() throws Exception {
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new FileReader("path.in"));
+        PrintWriter wr = new PrintWriter(new FileWriter("path.out"));
+
+		/* solution */
+        String[] line = br.readLine().split(" ");
+        int n = Integer.parseInt(line[0]);
+        int m = Integer.parseInt(line[1]);
+
+        char[][] mp = new char[n][m];
+        int x = 0, y = 0;
+
+        char[] rows = null;
+        for (int i = 0; i < n; i++) {
+            rows = br.readLine().toCharArray();
+            for (int j = 0; j < m; j++) {
+                mp[i][j] = rows[j];
+                if (rows[j] == 'A') {
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        int c = 0;
+        char ch = 'A';
+        boolean flag = true;
+        while(flag) {
+            flag = false;
+            if (mp[x][y] == ch) {
+                ch++;
+                c++;
+                flag = true;
+            }
+            if (x+1 < n && mp[x+1][y] == ch) { //bot
+                x++;
+            } else if (x-1 >= 0 && mp[x-1][y] == ch) { //top
+                x--;
+            } else if (y+1 < m && mp[x][y+1] == ch) { //right
+                y++;
+            } else if (y-1 >= 0 && mp[x][y-1] == ch) { //left
+                y--;
+            }
+        }
+
+        wr.println((c == 26 ? "YES" : "NO"));
+        wr.close();
+    }
 
 	public static void main(String[] args) {
 		
-		Solution s = new Solution();
+		ProblemA p = new ProblemA();
 		try {
-			s.solve();
+			p.solve();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 	}
 	
-}
-
-class Solution {
-	public void solve() throws Exception {
-		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedReader br = new BufferedReader(new FileReader("path.in")); 
-		PrintWriter wr = new PrintWriter(new FileWriter("path.out"));
-		
-		/* solution */
-		String[] line = br.readLine().split(" ");
-		int n = Integer.parseInt(line[0]);
-		int m = Integer.parseInt(line[1]);
-		
-		char[][] mp = new char[n][m];
-		int x = 0, y = 0;
-		
-		char[] rows = null;
-		for (int i = 0; i < n; i++) {
-			rows = br.readLine().toCharArray();
-			for (int j = 0; j < m; j++) {
-				mp[i][j] = rows[j];
-				if (rows[j] == 'A') {
-					x = i;
-					y = j;
-				}
-			}
-		}
-		int c = 0;
-		char ch = 'A';
-		boolean flag = true;
-		while(flag) {
-			flag = false;
-			if (mp[x][y] == ch) {
-				ch++;
-				c++;
-				flag = true;
-			}
-			if (x+1 < n && mp[x+1][y] == ch) { //bot
-				x++;
-			} else if (x-1 >= 0 && mp[x-1][y] == ch) { //top
-				x--;
-			} else if (y+1 < m && mp[x][y+1] == ch) { //right
-				y++;
-			} else if (y-1 >= 0 && mp[x][y-1] == ch) { //left
-				y--;
-			}
-		}
-		
-		wr.println((c == 26 ? "YES" : "NO"));
-		wr.close();
-	}
-	
-
 }
--- src/templates/Skeleton.java
+++ src/templates/Skeleton.java
@@ -1,15 +1,28 @@
 package templates;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
+import java.io.*;
 
 public class Skeleton {
+    public void solve() throws Exception {
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new FileReader("path.in"));
+        PrintWriter wr = new PrintWriter(new FileWriter("path.out"));
+
+		/* solution */
+
+
+
+
+
+
+
+		/* cleaning up */
+        wr.close();
+    }
 
 	public static void main(String[] args) {
 		
-		Solution s = new Solution();
+		Skeleton s = new Skeleton();
 		try {
 			s.solve();
 		} catch (Exception e) {
@@ -18,23 +31,4 @@ public class Skeleton {
 
 	}
 	
-}
-
-class Solution {
-	public void solve() throws Exception {
-		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedReader br = new BufferedReader(new FileReader("in.txt")); 
-		PrintStream ps = new PrintStream(System.out);
-		
-		/* solution */
-		
-		
-		
-		
-		
-		
-		
-		/* cleaning up */
-		ps.close();
-	}
 }
