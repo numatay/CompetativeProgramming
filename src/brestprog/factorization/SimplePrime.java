4fafe1067c8241e9962a328e68f22f03d0446141
Andrey Biryulin <andrey.biryulin@greenapple.kz>
2016-01-28 18:57:00 +0600

    Added isPrime() method

--- src/brestprog/factorization/SimplePrime.java
+++ src/brestprog/factorization/SimplePrime.java
@@ -2,8 +2,16 @@ package brestprog.factorization;
 
 public class SimplePrime {
     public static void main(String[] args) {
-        System.out.print("Hello");
+        System.out.print(isPrime(101));
     }
 
+    public static boolean isPrime(int x) {
+        for (int i = 2; i <= Math.sqrt(x); i++) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
