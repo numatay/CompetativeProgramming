016c501737bc8885253df01ba5d497540e13915b
Andrey Biryulin <andrey.biryulin@greenapple.kz>
2016-03-17 20:22:53 +0600

    Started problemA on CROC2016Q

---
+++ src/codeforces/croc2016q/ProblemA.java
@@ -0,0 +1,25 @@
+package codeforces.croc2016q;
+
+import java.util.Scanner;
+
+/**
+ * Created by Nurgissa on 3/17/2016.
+ */
+public class ProblemA {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        if (n > (a*b)) {
+            System.out.println(-1);
+            return;
+        }
+
+        int[][] m = new int[a][b];
+        while (n > 0) {
+
+        }
+    }
+}
