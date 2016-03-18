77b573f71840ff8c53dad2401dca305678a14cb2
macnur <nurgissa@gmail.com>
2016-03-18 22:29:13 +0600

    merged with problem B

---
+++ src/codeforces/croc2016q/ProblemB.java
@@ -0,0 +1,10 @@
+package codeforces.croc2016q;
+
+/**
+ * Created by macnur on 3/18/16.
+ */
+public class ProblemB {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
