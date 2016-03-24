fee492a63042a6ab21d532d0ffb4f129c6971d8c
macnur <nurgissa@gmail.com>
2016-03-25 00:14:32 +0600

    Added Rhs && Lhs version of choosing a pivot element in partition() method.

--- src/learn5minutes/sorting/SimpleQuickSort.java
+++ src/learn5minutes/sorting/SimpleQuickSort.java
@@ -1,5 +1,7 @@
 package learn5minutes.sorting;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import java.util.Arrays;
 
 /**
@@ -7,7 +9,7 @@ import java.util.Arrays;
  */
 public class SimpleQuickSort {
     public static void main(String[] args) {
-        int[] a = {8, 9, 3, 7};
+        int[] a = {12,32,43,34,23,23,34,45,5,66,76,7,56,45,34,3,4,32,3};
 
         quickSort(a);
 
@@ -20,14 +22,14 @@ public class SimpleQuickSort {
     }
 
     private static void quickSort(int[] lst, int l, int r) {
-        if (l >= r) { return; }
+        if (l >= r) return;
 
-        int idx = partition(lst, l, r);
+        int idx = partitionRhs(lst, l, r);
         quickSort(lst, l, idx-1);
         quickSort(lst, idx+1, r);
     }
 
-    private static int partition(int[] lst, int l, int r) {
+    private static int partitionRhs(int[] lst, int l, int r) {
         int pivot = lst[r];
 
         int tmp;
@@ -48,6 +50,31 @@ public class SimpleQuickSort {
 
         return i+1;
     }
+
+    private static int partitionLhs(int[] lst, int l, int r) {
+        int pivot = lst[l];
+
+        int tmp;
+        int i = r + 1;
+        for (int j = r; j > l; j--) {
+            if (lst[j] >= pivot) {
+                i--;
+                tmp = lst[j];
+                lst[j] = lst[i];
+                lst[i] = tmp;
+            }
+        }
+        tmp = lst[i-1];
+        lst[i-1] = pivot;
+        lst[l] = tmp;
+
+        return i-1;
+    }
+
+    private static int partitionMid(int[] lst, int l, int r) {
+        throw new NotImplementedException();
+    }
+
 
 
 }
