fa76db11dfaf11274e80a8cfa4b3eca09a99cf0a
Andrey Biryulin <andrey.biryulin@greenapple.kz>
2016-05-17 14:49:37 +0600

    Added INCOMPLETE implementation for problems #11 #114 [acmp]

---
+++ src/acmp/Problem11/Main.java
@@ -0,0 +1,65 @@
+package acmp.Problem11;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Nurgissa on 5/13/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int k = in.nextInt();
+        int n = in.nextInt();
+
+        int[] dp = new int[n];
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < i+k; j++) {
+                update(dp, j);
+            }
+            sum += dp[i];
+
+        }
+
+        for (int x : dp) {
+            out.println(x);
+        }
+
+        // release resources
+        out.close();
+    }
+
+    private static void update(int[] dp, int i) {
+        if (i < 0 || i >= dp.length) return;
+        dp[i]++;
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() { return Integer.parseInt(next()); }
+
+        public long nextLong() { return Long.parseLong(next()); }
+    }
+}
---
+++ src/acmp/Problem114/Main.java
@@ -0,0 +1,47 @@
+package acmp.Problem114;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Nurgissa on 5/16/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+
+
+        // release resources
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() { return Integer.parseInt(next()); }
+
+        public long nextLong() { return Long.parseLong(next()); }
+    }
+}
