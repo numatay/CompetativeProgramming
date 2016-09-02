28739b034a1417059c81b0ec30f3b85ab75a2d79
umatayn <umatayn@gmail.com>
2016-09-02 15:25:09 +0600

    Added AC problem #460

---
+++ src/mccme/graph/Problem460.java
@@ -0,0 +1,73 @@
+package mccme.graph;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by @umatayn on 9/2/2016.
+ */
+public class Problem460 {
+
+    public static void main(String[] args) {
+        // initialize resources
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        // solution
+        int n = in.nextInt();
+        int[][] a = new int[n][n];
+
+        // reading adj. matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = in.nextInt();
+                if (a[i][i] == 1) {
+                    System.out.println("NO");
+                    return;
+                }
+            }
+        }
+
+        // checking validity of adj. matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                if (a[i][j] != a[j][i]) {
+                    System.out.println("NO");
+                    return;
+                }
+            }
+        }
+
+        System.out.println("YES");
+
+        // release resources
+        out.close();
+    }
+
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() { return Integer.parseInt(next()); }
+
+        public long nextLong() { return Long.parseLong(next()); }
+    }
+}
+
