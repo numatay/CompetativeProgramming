3c6f56eb89792f205bbfe92f38de8cb0f6a607a0
umatayn <umatayn@gmail.com>
2016-09-05 09:53:09 +0600

    Added AC problems #467, #468, #469, #470, #471

---
+++ src/mccme/graph/Problem471.java
@@ -0,0 +1,60 @@
+//package mccme.graph;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by @umatayn on 9/2/2016.
+ */
+public class Problem471 {
+    public static void main(String[] args) {
+        // initialize resources
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        // solution
+        int n = in.nextInt();
+        int[] a = new int[n+1];
+
+        int m = in.nextInt();
+
+        for (int i = 0; i < m; i++) {
+            int u = in.nextInt();
+            int v = in.nextInt();
+            a[u]++;
+            a[v]++;
+        }
+
+        for (int j = 1; j <= n; j++) {
+            System.out.println(a[j]);
+        }
+        // release resources
+        out.close();
+    }
+
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() { return Integer.parseInt(next()); }
+
+        public long nextLong() { return Long.parseLong(next()); }
+    }
+}
