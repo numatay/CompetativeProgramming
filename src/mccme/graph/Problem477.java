257da24f335458b815f5c1521a92d520e260daf8
umatayn <umatayn@gmail.com>
2016-09-05 11:11:06 +0600

    Added AC problems #477, #478

---
+++ src/mccme/graph/Problem477.java
@@ -0,0 +1,67 @@
+package mccme.graph;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * Created by @umatayn on 9/2/2016.
+ */
+public class Problem477 {
+    public static void main(String[] args) {
+        // initialize resources
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        // solution
+        int n = in.nextInt();
+        int[][] a = new int[n+1][n+1];
+
+        int m = in.nextInt();
+
+        for (int i = 0; i < m; i++) {
+            int u = in.nextInt();
+            int v = in.nextInt();
+            a[u][v]++;
+            a[v][u]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i != j && a[i][j] == 0) {
+                    System.out.println("NO");
+                    return;
+                }
+            }
+        }
+        System.out.println("YES");
+
+        // release resources
+        out.close();
+    }
+
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() { return Integer.parseInt(next()); }
+
+        public long nextLong() { return Long.parseLong(next()); }
+    }
+}
