190c026137cb51978735bc81232726453985f285
umatayn <umatayn@gmail.com>
2016-10-13 10:55:29 +0600

    Added AC problem #172

--- src/leetcode/problem172/Solution.java
+++ src/leetcode/problem172/Solution.java
@@ -10,32 +10,12 @@ public class Solution {
     }
 
     public int trailingZeroes(int n) {
-        long m = 1, c = 0;
-        for (long i = 1, j = 1; i <= n; i++, j++) {
-            j = i;
-            System.out.println("inside loop m:" + m + " and i:" + i + " and j:" + j + " c:" + c);
-            if (j % 10 == 0) {
-                while(j % 10 == 0) {
-                    c++;
-                    j /= 10;
-                    System.out.println("cleaning up");
-                }
-                continue;
-            }
-            j %= 100;
-
-            if (m % 10 == 0) {
-                c++;
-                m /= 10;
-                //m %= 50;
-            }
-            m *= j;
-            m %= 1000;
-
+        int sum = 0;
+        while (n > 0) {
+            sum += n / 5;
+            n /= 5;
         }
-        System.out.println(m);
-        if (m % 10 == 0) c++;
 
-        return (int)c;
+        return sum;
     }
 }
